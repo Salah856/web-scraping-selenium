d8c211be12b8311e0c7897703984dc3cbfebe931
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-05-21 14:56:17 +0200

    web scraping using selenium and bs4

---
+++ scrapy.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+# Establish chrome driver and go to report site URL
+url = "https://reportdata.mytestsite.com/transactionSearch.jsp"
+driver = webdriver.Chrome()
+driver.get(url) 
+
+
+# Switch to iframe where form is
+frame_ref = driver.find_elements_by_tag_name("iframe")[0]
+iframe = driver.switch_to.frame(frame_ref)
+
+# Find the Customer ID field and populate it
+element = driver.find_element_by_name("custId")
+element.send_keys(custId)  # send a test id
+
+# Find and select the date drop-downs
+select = Select(driver.find_element_by_name("fromMonth"))
+select.select_by_visible_text(from_month)
+select = Select(driver.find_element_by_name("fromYear"))
+select.select_by_visible_text(from_year)
+select = Select(driver.find_element_by_name("toMonth"))
+select.select_by_visible_text(to_month)
+select = Select(driver.find_element_by_name("toYear"))
+select.select_by_visible_text(to_year)
+
+driver.find_element_by_xpath("/html/body/table/tbody/tr[2]/td[1]/table[3]/tbody/tr[2]/td[2]/input").click()
+
+
+# Loop through transactions and count
+links = driver.find_elements_by_tag_name('a')
+link_urls = [link.get_attribute('href') for link in links]
+thisCount = 0
+isFirst = 1
+for url in link_urls:
+if (url.find("GetXas.do?processId") >= 0):  # URL to link to transactions
+       	if isFirst == 1:  # already expanded +
+              	isFirst = 0
+else:
+       	driver.get(url)  # collapsed +, so expand
+# Find closest element to URL element with correct class to get tran type                            tran_type=driver.find_element_by_xpath("//*[contains(@href,'/retail/transaction/results/GetXas.do?processId=-1')]/following::td[@class='txt_75b_lmnw_T1R10B1']").text
+              # Get transaction status
+              status = driver.find_element_by_class_name('txt_70b_lmnw_t1r10b1').text
+              # Add to count if transaction found
+              if (tran_type in ['Move In','Move Out','Switch']) and 
+(status == "Complete"):
+                    thisCount += 1
+                    
+                    
+#element = WebDriverWait(driver, 10). until(EC.presence_of_element_located((By.ID, "theFirstLabel"))) 
+                    
+                    
